c5e36f3efab88be3087ce12058cf20d5ce5c60f9
Guclu Ozturk <gucluozturk@gmail.com>
2015-10-11 13:27:56 +0300

    Take description field into account when autocomplete
    	* Also make auto complete field built from template
    	* instead of static from code

--- web/search_indexes.py
+++ web/search_indexes.py
@@ -5,7 +5,7 @@ from web.models import Petition
 
 class PetitionIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    content_auto = indexes.EdgeNgramField(model_attr='name')
+    content_auto = indexes.EdgeNgramField(model_attr='name', use_template=True)
 
     def get_model(self):
         return Petition
