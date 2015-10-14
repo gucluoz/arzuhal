f89d7ff9654b62784627eb979663595b485b4bca
Guclu Ozturk <gucluozturk@gmail.com>
2015-10-14 16:16:29 +0300

    Write crawler views

--- web/urls.py
+++ web/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
   url(r'^q/(?P<q>[a-zA-Z0-9\%\ ]+)$', views.indexsearch, name='indexsearch'),
   url(r'^a/', views.autoComplete, name='autocomplete'),
   url(r'^list/$', views.PetitionListView.as_view(), name='list'),
+  url(r'^crawl/$', views.crawl, name='crawl'),
   url(r'^detail/(?P<name>[a-z_]+)$', views.detailByName, name='detailbyName'),
   url(r'^d/(?P<ticket>[A-Za-z0-9]{32})', views.download, name='download'),
 ]
--- web/views.py
+++ web/views.py
@@ -9,7 +9,7 @@ from django.core.servers.basehttp import FileWrapper
 from haystack.query import SearchQuerySet
 from django.core.urlresolvers import reverse
 
-from .models import Petition, Template
+from .models import Petition, Template, Subject
 
 
 class PetitionListView(generic.ListView):
@@ -73,3 +73,104 @@ def detailByName(request, name):
       context['first_url'] = reverse('download', args=[context['templates_ordered'].first().downloadticket])
 
   return render(request, 'web/petitiondetail.html.j2', context)
+
+from bs4 import BeautifulSoup as Bs
+import re, urllib, binascii, os, urlparse, csv
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+def generateSoup(fileName):
+  f = open(fileName)
+  text = f.read()
+  soup = Bs(text,from_encoding='iso-8859-9')
+  f.close()
+  return soup
+
+def parseListtd(tdTag):
+  return 'http://www.dilekceyaz.net/' + tdTag.a.get('href')
+
+def parseAllList(soup):
+  res = []
+  for td in soup.find_all('td'):
+    res.append(parseListtd(td))
+
+  return res
+
+def parseDetail(soup):
+  #subject = re.findall(u'(?<=Di\xf0er  )(.*)(?= \xddle \xddlgili Dilek\xe7e \xd6rnekleri)',soup.find(id='MainContent_HyperLink1').text)[0]
+  subject = soup.find(id='MainContent_HyperLink1').text.split(' ')[2]
+  link = soup.find(id='MainContent_dilekce_htmgoster').get('src').replace('dilekceler_htm','http://www.dilekceyaz.net/dilekceler').replace('.htm','.doc')
+  print('-parseDetail() -> '+subject)
+  ret = {
+    'text' : soup.find(id='MainContent_konu').text.replace('\n','').strip(),
+    'subject' : subject.replace('\n','').strip(),
+    'link' : link.replace('\n','').strip()
+  }
+  print('+parseDetail() -> '+ret['subject'])
+
+  return ret;
+
+def downloadContent(url):
+  try:
+    sock = urllib.urlopen(url.encode('iso-8859-9'))
+    return sock.read()
+  finally:
+    sock.close()
+
+def downloadFile(url, filename):
+    urllib.urlretrieve(url.encode('iso-8859-9'),'petitions/' + filename)
+
+def generateFileName():
+  return binascii.hexlify(os.urandom(16))
+
+def appendToFile(detail, name, extension):
+  sub = Subject.objects.filter(name=detail['subject'])
+  if not sub:
+    sub = Subject(name=detail['subject'])
+    sub.save()
+    print('Subject save -> DONE')
+
+  pet = Petition()
+  pet.name = detail['text']
+  pet.subject = Subject.objects.filter(name=detail['subject']).get()
+  pet.publishdate = timezone.now()
+  pet.publishedby = User.objects.all()[0]
+  pet.description = detail['text']
+  pet.save()
+  print('Petition save -> DONE')
+  template = Template()
+  template.version = 1
+  template.filename = name
+  template.extension = extension
+  template.petition = pet
+  template.publishdate = timezone.now()
+  template.publishedby = User.objects.all()[0]
+  template.save()
+  print('Template save -> DONE')
+
+def crawl(request):
+  petList = parseAllList(generateSoup('/Users/gucluozturk/Projects/dilekce_crawler/crawler/list.xml'))
+  for pet in petList:
+    try:
+      print('FETCHING :' + pet + '...')
+      detailText = downloadContent(pet)
+      print('DONE')
+      detailSoup = Bs(detailText, from_encoding='iso-8859-9')
+      detail = parseDetail(detailSoup)
+
+      filename = generateFileName()
+      split = urlparse.urlsplit(detail['link'])
+      fileextension = split.path.split('/')[-1].split('.')[-1]
+      print('DOWNLOADING :' + detail['link'] + '...')
+      downloadFile(detail['link'],filename)
+      print('DONE')
+
+      appendToFile(detail,filename, fileextension)
+
+      print(detail['subject'])
+      print(detail['text'])
+      print(filename + '.' + fileextension)
+      print('-------------------------------')
+    except:
+      print('************ ERROR ************')
+  return HttpResponse(status=200)
