744ecd7420ae4f741e764434a1c9bdd77f354b66
Guclu Ozturk <gucluozturk@gmail.com>
2015-10-24 11:31:28 +0300

    Remove js onclick links from subject list and put solid urls

--- web/urls.py
+++ web/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name='index'),
-  url(r'^q/(?P<q>[a-zA-Z0-9\%\ ]+)$', views.indexsearch, name='indexsearch'),
+  url(u'^q/(?P<q>[a-zA-ZğüşıöçĞÜŞİÖÇ0-9\%\ ]+)$', views.indexsearch, name='indexsearch'),
   url(r'^a/', views.autoComplete, name='autocomplete'),
   url(r'^list/$', views.PetitionListView.as_view(), name='list'),
   url(r'^detail/(?P<name>[a-z0-9_]+)$', views.detailByName, name='detailbyName'),
