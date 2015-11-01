353fee3216cef5b786ec488e966fed0afb438147
Guclu Ozturk <gucluozturk@gmail.com>
2015-11-01 16:28:09 +0300

    Add robots.txt

--- arzuhal/urls.py
+++ arzuhal/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from sitemaps import ArzuhalSitemap
+from django.views.generic import TemplateView
 
 sitemaps = {
   'arzuhal' : ArzuhalSitemap()
@@ -12,4 +13,6 @@ urlpatterns = [
     url(r'^web/', include('web.urls')),
     url(r'^harita\.xml$', sitemap, {'sitemaps': sitemaps},
       name='django.contrib.sitemaps.views.sitemap'),
+    url(r'^robots\.txt$', TemplateView.as_view(
+      template_name='robots.txt', content_type='text/plain')),
 ]
